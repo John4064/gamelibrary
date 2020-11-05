b462c2d367c4610efce65ee8d11ed52d6a15f0ae
John4064 <jparkhurst120@gmail.com>
2020-11-05 13:50:33 -0500

    Updated Game to feature Movement
    Developed basic left and right movement now developing a basic shoot mechanic for the game

--- Space Invader/main.py
+++ Space Invader/main.py
@@ -2,25 +2,31 @@
 #Setting some global variables
 import pygame
 pygame.init()
-win = pygame.display.set_mode((720,720))
+winHeight=720
+winWidth = 720
+win = pygame.display.set_mode((winHeight,winWidth),display=1)
+count = 0
 pygame.display.set_caption("Space Invader")
 clock = pygame.time.Clock()
-
-class player(object):
-    def __init__(self,x,y,width,height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.vel = 5
 class game():
+    def input(self,keys,player):
+        global winHeight,winWidth,count
+        if (keys[pygame.K_a] or keys[pygame.K_LEFT])and player.x>(0):
+            player.x-=player.vel
+        elif (keys[pygame.K_d] or keys[pygame.K_RIGHT])and player.x<(winWidth-player.width):
+            player.x+=player.vel
+        elif(keys[pygame.K_SPACE]):
+            count+=1
+            print(count)
+        return
     def __init__(self):
         run = True
 
-        player = spaceship(50, 100, 50, 100)
+        player = spaceship(300, 600, 80, 80)
         while run:
-            keys = pygame.key.get_pressed()
             clock.tick(60)
+            keys = pygame.key.get_pressed()
+            self.input(keys,player)
             self.redrawGame(win,player)
 
             for event in pygame.event.get():
@@ -30,13 +36,24 @@ class game():
     def redrawGame(self,win,player):
         win.blit(pygame.image.load('bg.png'), (0, 0))
         #man.draw(win)
-        player.draw(win)
+        player.draw(win,player.x,player.y)
         pygame.display.update()
-class spaceship():
-    def draw(self,win):
-        pygame.draw.rect(win,(255,0,0),(50,100,50,100))
+class spaceship(pygame.sprite.Sprite):
+    def draw(self,win,x,y):
+        self.x = x
+        self.y =y
+        #pygame.draw.rect(win,(255,0,0),(self.x,self.y,self.width,self.height))
+        self.image = pygame.image.load('spaceship.png')
+        self.rect = self.image.get_rect()
+        win.blit(self.image,(self.x,self.y))
     def __init__(self,x,y,width,height):
-        print(1)
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 7
+        #self.image.fill((0,255,255))
 class meteor():
     def __init__(self):
         print(1)
